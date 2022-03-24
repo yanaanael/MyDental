fd7e8abec4fb5469a5dda2d22a17cdbd192a5b1e
Anael yana <yana211293@gmail.com>
2022-03-24 14:00:01 +0200

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,17 @@
+import "./Header.css"
+import logo from "../../images/mydental-logo.png"
 
 function Header(props) {
 
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="headerContainer">
+      {/* <p className="headerTitle">MyDental</p> */}
+      <img className="logo" src={logo}/>
+      <div className="nav-links">
+        <a href="#" className="nav-link">אודות</a>
+        <a href="#" className=" primaryButton" >הרשם \ הכנס</a>
+       
+      </div>
     </div>
   );
 }
